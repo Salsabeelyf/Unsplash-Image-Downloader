bffaad7f8b5e7e56710ac940cd18f643a8e813f7
Salsabeelyf <salsabeelsay@gmail.com>
2024-08-11 15:15:27 +0100

    Add API_based functionality

--- api_helpers.py
+++ api_helpers.py
@@ -3,10 +3,25 @@ import constants as c
 import classes
 
 def get_images_info():
-    page_no = 1
-    resp = requests.get(f'https://unsplash.com/napi/search/photos?page={page_no}&per_page={c.no_of_images}&query={c.search_query}').json()
-    images = resp.get('results')
-    images_list = [classes.Image(id = image.get('id'),
-                                 title = str.replace(image.get('alt_description'),' ','-'),
-                                 download_link = image.get('links').get('download')) for image in images]
-    return images_list
+    page_no = 0
+    images_list = []
+
+    # Get images and save them to the list until reach the desired number of images
+    while(len(images_list) < c.no_of_images ):
+        page_no = page_no + 1
+
+        # Get images per page
+        resp = requests.get(f'https://unsplash.com/napi/search/photos?page={page_no}&per_page={c.no_of_images}&query={c.search_query}').json()
+        
+        # Extract images from response
+        images = resp.get('results')
+
+        # Save images ids, titles, and download links if only free
+        list = [classes.Image(id = image.get('id'),
+                              title = str.replace(image.get('alt_description'),' ','-'),
+                              download_link = image.get('links').get('download')) for image in images if image.get('premium') == False]
+        
+        # Add images to images list
+        images_list.extend(list)
+
+    return images_list[:c.no_of_images]
