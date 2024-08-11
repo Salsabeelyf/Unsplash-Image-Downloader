df7fe765b45ec6f3d3c2ea74c75c87beaa8aa4fb
Salsabeelyf <salsabeelsay@gmail.com>
2024-08-11 11:02:42 +0100

    Take search keyword and number of images as arguments

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+import sys
 import constants as c
 import helpers as h
 
 
 def run():
+    # Check if argument provided
+    if len(sys.argv) == 1:
+        print('Please Provide 2 arguments, search keywork and number of images\nFor example: py main.py "water drop" 5')
+        exit()
+
+    c.search_query = sys.argv[1]
+    c.no_of_images = int(sys.argv[2])
+
     # Seach for images and get a list of them as html nodes
     images = h.search_for_images(search_query=c.search_query, no_of_images=c.no_of_images)
 
